249931ca5a859b2b16daf5ebc15d9a2867c7c155
BanoskiGG <philipteves2004@gmail.com>
2025-11-06 11:56:23 +0800

    Implemented participant registration

--- techfest_registration.py
+++ techfest_registration.py
@@ -1,1 +1,13 @@
 # TechFest Registration System A Python program that records event participants and analyzes track diversity.
+
+
+# Task 1: Registration Setup
+print("Welcome to SMIT TechFest!")
+print("Event organized by Philip Andrei Teves of APPDAET BTCS2\n")
+
+# Ask user how many participants will join
+num_participants = int(input("How many participants will register? "))
+
+if num_participants <= 0:
+    print("Invalid number of participants.")
+else:
