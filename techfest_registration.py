19c9b6a9c9ad6ea8ae02a89d95b701574a0de5e6
BanoskiGG <philipteves2004@gmail.com>
2025-11-06 12:03:51 +0800

    Implemented the collection of participant's information

--- techfest_registration.py
+++ techfest_registration.py
@@ -11,3 +11,20 @@ num_participants = int(input("How many participants will register? "))
 if num_participants <= 0:
     print("Invalid number of participants.")
 else:
+    # Task 2: Collect Participant Information
+    participants = []
+
+    for i in range(num_participants):
+        print()
+        name = input("Enter participant name: ")
+        track = input("Enter chosen track: ")
+
+        record = {"name": name, "track": track}
+        participants.append(record)
+
+    print("\nRegistered Participants:")
+    for i in range(num_participants):
+        data = participants[i]
+        print(f"{i + 1}. {data['name']} - {data['track']}")
+    print()
+
